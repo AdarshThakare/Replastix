475c926e2066fe1a9d2aee26c43bfcedbe59f2f9
Adarsh Thakare <adarshythakare@gmail.com>
2025-07-09 20:11:57 +0530

    adnroid modification

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.replastix',
+  appName: 'Repastix',
+  webDir: 'dist'
+};
+
+export default config;
